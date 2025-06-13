a6a427006491970b1da9d21e48b1eebc6087c2fd
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2025-06-13 15:32:58 -0600

    [MA-02] Feature: Improvements and Updates in Several Sections [v1]

--- transpose.py
+++ transpose.py
@@ -18,9 +18,9 @@ def T(X: List[List[int]]) -> List[List[int]]:
     # return [list(row) for row in zip(*X)]
     X_A: List[List[int]] = []
     for i in range(len(X)):
-        AUX = []
-        for row in X:
-            AUX.append(row[i])
-        X_A.append(AUX)
+        for j in range(len(X[i])):
+            if len(X_A) < j + 1:
+                X_A.append([])
+            X_A[j].append(X[i][j])
 
     return X_A
